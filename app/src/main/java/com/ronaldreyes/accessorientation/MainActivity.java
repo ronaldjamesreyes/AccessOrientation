a24fed8064928880dd4d2b7f26536e0317249c57
Ronald James Reyes <48842512+ronaldreyes224@users.noreply.github.com>
2020-01-14 16:31:42 -0800

    Added correct/incorrect method

--- app/src/main/java/com/ronaldreyes/accessorientation/MainActivity.java
+++ app/src/main/java/com/ronaldreyes/accessorientation/MainActivity.java
@@ -3,12 +3,35 @@ package com.ronaldreyes.accessorientation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText textField;
+    private TextView answerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textField = findViewById(R.id.text_field);
+        Button submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(this);
+        answerLabel = findViewById(R.id.answer_label);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        String textFieldText= textField.getText().toString();
+
+        if (textFieldText.equalsIgnoreCase("42")){
+            answerLabel.setText(R.string.answer_label_text_correct);
+        } else{
+            answerLabel.setText(R.string.answer_label_text_incorrect);
+        }
     }
 }
